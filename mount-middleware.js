c19188cf7cb336eee70d9acea38591aa4cc1e109
Christopher Lamm <chris@theantichris.com>
2020-10-27 12:59:19 -0400

    clean up

--- mount-middleware.js
+++ mount-middleware.js
@@ -2,16 +2,16 @@ import express from "express"
 import { join } from "path"
 
 const attachLocals = require("./attach-locals")
-const lastResortErrorHandler = require("./last-resort-error-handler")
 const primeRequestContext = require("./prime-request-context")
+const lastResortErrorHandler = require("./last-resort-error-handler")
 
 function mountMiddleware(app, env){
-  app.use(lastResortErrorHandler)
-  app.use(primeRequestContext)
   app.use(attachLocals)
+  app.use(primeRequestContext)
+  app.use(lastResortErrorHandler)
 
   app.use(
-    express.static(join(__dirname, "..", "public"), { maxAge: 86400000})
+    express.static(join(__dirname, "..", "public"), { maxAge: 86400000 })
   )
 }
 
